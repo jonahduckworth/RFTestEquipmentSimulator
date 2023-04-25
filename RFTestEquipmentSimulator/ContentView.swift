68a2cf80608360acaf5542eab917ded8087efbd8
jones <35585020+fleeced@users.noreply.github.com>
2023-04-25 12:19:34 -0600

    start of swift app. window with tab view pops up on build

---
+++ RFTestEquipmentSimulator/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  RFTestEquipmentSimulator
+//
+//  Created by Jonah Duckworth on 2023-04-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            SignalGeneratorView()
+                .tabItem {
+                    Label("Signal Generator", systemImage: "waveform.path.ecg")
+                }
+            
+            SpectrumAnalyzerView()
+                .tabItem {
+                    Label("Spectrum Analyzer", systemImage: "waveform.path")
+                }
+            
+            NetworkAnalyzerView()
+                .tabItem {
+                    Label("Network Analyzer", systemImage: "network")
+                }
+        }
+    }
+}
+
+struct SignalGeneratorView: View {
+    var body: some View {
+        Text("Signal Generator")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct SpectrumAnalyzerView: View {
+    var body: some View {
+        Text("Spectrum Analyzer")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct NetworkAnalyzerView: View {
+    var body: some View {
+        Text("Network Analyzer")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
---
+++ RFTestEquipmentSimulator/RFTestEquipmentSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RFTestEquipmentSimulatorApp.swift
+//  RFTestEquipmentSimulator
+//
+//  Created by Jonah Duckworth on 2023-04-25.
+//
+
+import SwiftUI
+
+@main
+struct RFTestEquipmentSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
